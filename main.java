c4c7036d62ce95958cf3382a17d5add6df12207a
IvanNegrete <54756123+IvanNegrete@users.noreply.github.com>
2020-08-17 18:02:43 -0600

    Juego contra PC
    El juego determina quien empieza al azar, la PC es el jugador 2

--- main.java
+++ main.java
@@ -4,40 +4,59 @@ import java.util.Scanner;
 
 public class main {
 
+	static boolean turnoX = true;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		boolean turnoX = true;
-		boolean victoria = false;
+		//Se determina el jugador que inicia aleatoriamente
+		if((int)(Math.random() * ((2)+1) + 1) == 1) {
+			turnoX = true;
+		}else {
+			turnoX = false;
+		}
 		Scanner leer = new Scanner (System.in);
 		while (true) {
+			//Pide ingresar las coordenadas para colocar la marca
 			System.out.print("Turno de: ");
 			if(turnoX) {
-				System.out.println("x");
+				System.out.println("Jugador (x)");
 			}else {
-				System.out.println("o");
+				System.out.println("PC (o)");
 			}
 			pintarTablero();
-			System.out.println("Ingrese la columna");
-			int x = leer.nextInt();
-			System.out.println("Ingrese la fila");
-			int y = leer.nextInt();
-			if((x>=1 && x<=3) && (y>=1 && y<=3) && tablero[y][x]==0) {
-				if(turnoX) {
+			if(turnoX) {
+				System.out.println("Ingrese la columna");
+				int x = leer.nextInt();
+				System.out.println("Ingrese la fila");
+				int y = leer.nextInt();
+				if((x>=1 && x<=3) && (y>=1 && y<=3) && tablero[y][x]==0) {
 					tablero[y][x]=4;
 					turnoX=false;
 				}else {
-					tablero[y][x]=5;
-					turnoX=true;
+					System.out.println("Posiscion invalida");
+					System.out.println("");
 				}
 			}else {
-				System.out.println("Posiscion invalida");
+				turnoPC();
 			}
-			if(revisarEmpate()) {
+			if(revisarVictoria() || revisarEmpate()) {
 				volver_a_jugar();
 			}
 		}
 	}
- 
+	
+	public static void turnoPC() {
+		boolean ocupado = false;
+		do{
+			int x = (int)(Math.random() * ((3-1)+1) + 1), y = (int)(Math.random() * ((3-1)+1) + 1);
+			if(tablero[x][y] == 0) {
+				tablero[x][y] = 5;
+				ocupado = true;
+				turnoX = true;
+			}
+		}while(!ocupado);
+	}
+	
 	//se crea el tablero de juego
 	static int tablero [][] = 
 			{{7,1,2,3},
@@ -85,6 +104,11 @@ public class main {
 						tablero[i][e]=0;
 					}
 				}
+				if((Math.random() * ((2-1)+1) + 1) == 1) {
+					turnoX = true;
+				}else {
+					turnoX = false;
+				}
 				error = false;
 				break;
 			case 2:
@@ -92,10 +116,50 @@ public class main {
 				break;
 				default:
 					System.out.println("Opcion no valida");
+					System.out.println("");
 					error = true;
 					break;
 			}
 		}
+	}
+	
+	public static boolean revisarVictoria() {
+		boolean horizontal = false, vertical = false, diagonal = false;
+		int ultimoJugado = 4;
+		for(int i = 1; i < tablero.length; i ++) {
+			if(tablero[i][1] != 0 && tablero[i][1] == tablero[i][2] && tablero[i][1] == tablero[i][3]) {
+				horizontal = true;
+			}else{
+				horizontal = false;
+			}
+			if(tablero[1][i] != 0 && tablero[1][i] == tablero[2][i] && tablero[1][i] == tablero[3][i]) {
+				vertical = true;
+			}else {
+				vertical = false;
+			}
+			if(horizontal || vertical) {
+				ultimoJugado = tablero[i][1];
+				i = tablero.length + 1;
+			}
+		}
+		if((tablero[1][1] != 0 && tablero[1][1] == tablero[2][2] && tablero[1][1] == tablero[3][3]) || (tablero[3][1] != 0 && tablero[3][1] == tablero[2][2] && tablero[3][1] == tablero[1][3])) {
+			diagonal = true;
+		}else {
+			diagonal = false;
+		}
+		if(horizontal || vertical || diagonal) {
+			pintarTablero();
+			System.out.println("");
+			System.out.print("Ganó el jugador ");
+			if(ultimoJugado == 4) {
+				System.out.println("x");
+			}else {
+				System.out.println("o");
+			}
+			System.out.println("");
+			return true;
+		}
+		return false;
 	}
 	
 	public static boolean revisarEmpate() {
@@ -106,6 +170,8 @@ public class main {
 				}
 			}
 		}
+		pintarTablero();
+		System.out.println("Hubo un empate");
 		return true;
 	}
 	
